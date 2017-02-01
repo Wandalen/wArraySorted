7aa4ca8fa1cceabbdfcc6e5260bd49164f0ac76f
wandalen <wandalen.me@gmail.com>
2017-02-01 21:49:32 +0400

    adjust sample & add tests

--- sample/Sample.js
+++ sample/Sample.js
@@ -3,3 +3,31 @@ if( typeof module !== 'undefined' )
 {
   require( 'wArraySorted' );
 }
+
+var _ = wTools;
+var arr = [ 1,2,5,9 ];
+
+var e = 0
+var i = _.arraySortedLookUpIndex( arr,e );
+console.log( 'arraySortedLookUpIndex',e,i );
+// arraySortedLookUpIndex 0 0
+
+var e = 1
+var i = _.arraySortedLookUpIndex( arr,e );
+console.log( 'arraySortedLookUpIndex',e,i );
+// arraySortedLookUpIndex 1 0
+
+var e = 4
+var i = _.arraySortedLookUpIndex( arr,e );
+console.log( 'arraySortedLookUpIndex',e,i );
+// arraySortedLookUpIndex 4 2
+
+var e = 5
+var i = _.arraySortedLookUpIndex( arr,e );
+console.log( 'arraySortedLookUpIndex',e,i );
+// arraySortedLookUpIndex 5 2
+
+var e = 10
+var i = _.arraySortedLookUpIndex( arr,e );
+console.log( 'arraySortedLookUpIndex',e,i );
+// arraySortedLookUpIndex 10 4
