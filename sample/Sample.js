7a068cf8358112424db31fa95448c71e6882e47f
volodymyrmigdal <volodymyr.migdal@gmail.com>
2017-02-06 17:03:13 +0200

    Sample update output

--- sample/Sample.js
+++ sample/Sample.js
@@ -9,25 +9,25 @@ var arr = [ 1,2,5,9 ];
 
 var e = 0
 var i = _.arraySortedLookUpIndex( arr,e );
-console.log( 'arraySortedLookUpIndex',e,i );
-// arraySortedLookUpIndex 0 0
+console.log( 'arraySortedLookUpIndex(',e,') :',i );
+// arraySortedLookUpIndex( 0 ) : -1
 
 var e = 1
 var i = _.arraySortedLookUpIndex( arr,e );
-console.log( 'arraySortedLookUpIndex',e,i );
-// arraySortedLookUpIndex 1 0
+console.log( 'arraySortedLookUpIndex(',e,') :',i );
+// arraySortedLookUpIndex( 1 ) : 0
 
 var e = 4
 var i = _.arraySortedLookUpIndex( arr,e );
-console.log( 'arraySortedLookUpIndex',e,i );
-// arraySortedLookUpIndex 4 2
+console.log( 'arraySortedLookUpIndex(',e,') :',i );
+// arraySortedLookUpIndex( 4 ) : -1
 
 var e = 5
 var i = _.arraySortedLookUpIndex( arr,e );
-console.log( 'arraySortedLookUpIndex',e,i );
-// arraySortedLookUpIndex 5 2
+console.log( 'arraySortedLookUpIndex(',e,') :',i );
+// arraySortedLookUpIndex( 5 ) : 2
 
 var e = 10
 var i = _.arraySortedLookUpIndex( arr,e );
-console.log( 'arraySortedLookUpIndex',e,i );
-// arraySortedLookUpIndex 10 4
+console.log( 'arraySortedLookUpIndex(',e,') :',i );
+// arraySortedLookUpIndex( 10 ) : -1
