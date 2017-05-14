d6dc3e52fcbc7b7e9a75f69a995d2e68f4edf950
wandalen <wandalen.me@gmail.com>
2017-05-14 23:01:28 +0400

    experiment tag for arraySortedLookUpIntervalNarrowest

--- staging/abase/component/ArraySorted.s
+++ staging/abase/component/ArraySorted.s
@@ -803,7 +803,7 @@ var Proto =
   arraySortedLookUpClosest : arraySortedLookUpClosest,
 
   arraySortedLookUpInterval : arraySortedLookUpInterval,
-  arraySortedLookUpIntervalNarrowest : arraySortedLookUpIntervalNarrowest,
+  arraySortedLookUpIntervalNarrowest : arraySortedLookUpIntervalNarrowest, /* experimental */
   arraySortedLookUpEmbrace : arraySortedLookUpEmbrace,
 
   _arraySortedLeftMostIndex : _arraySortedLeftMostIndex,
